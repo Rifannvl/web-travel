ca6325e8dbee1d34f8d895184b689d8c29defec6
Rifan <rifan@example.com>
2024-09-28 20:51:14 +0700

    menambahkan hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,15 @@
+import React from "react";
+import Baselayout from "../layout/Baselayout";
+import HeroSection from "../components/HeroSection";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Home</h1>;
+  return (
+    <div>
+      <Baselayout>
+        <HeroSection />
+
+        <h1 className="text-3xl font-bold ">Home</h1>
+      </Baselayout>
+    </div>
+  );
 }
