61fa581cf2e23e8684d159539d41c0972204c61b
Rifan <rifan@example.com>
2024-09-28 23:48:01 +0700

    menambahkan bento dan about ke home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Baselayout from "../layout/Baselayout";
 import HeroSection from "../components/HeroSection";
-
+import BentoImage from "../components/BentoImage";
+import About from "../components/About";
 export default function Home() {
   return (
     <div>
       <Baselayout>
         <HeroSection />
-
-        <h1 className="text-3xl font-bold ">Home</h1>
+        <BentoImage />
+        <About />
       </Baselayout>
     </div>
   );
