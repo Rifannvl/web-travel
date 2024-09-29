fc830a360033f5636f8bd64be93b539a94c41cbb
Rifan <rifan@example.com>
2024-09-30 01:48:45 +0700

    membuat footer menjadi di bawah

--- src/layout/Baselayout.jsx
+++ src/layout/Baselayout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+
 export default function Baselayout({ children }) {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <main>{children}</main>
+      <main className="flex-grow">{children}</main>
       <Footer />
     </div>
   );
