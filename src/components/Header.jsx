1d0d7e9c0126a21822613f67be84b28bd3650d93
Rifan <rifan@example.com>
2024-10-02 20:28:34 +0700

    membuat agar di scroll bg pada navbar berubah dan juga pada route lain

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebook,
@@ -6,18 +6,48 @@ import {
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const location = useLocation(); // Hook untuk mendapatkan lokasi saat ini
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 50) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // Menentukan apakah berada di halaman beranda
+  const isHomePage = location.pathname === "/";
+
+  // Mengatur latar belakang navbar
+  const navbarBackground = isHomePage
+    ? isScrolled
+      ? "bg-black"
+      : "bg-transparent"
+    : "bg-black";
+
   return (
-    <div className="text-white">
-      <header className="container mx-auto flex justify-between items-center p-4 shadow-lg fixed top-0 left-0 w-full z-50">
+    <div
+      className={`text-white fixed w-full z-50 ${navbarBackground} transition-all duration-300`}
+    >
+      <header className="container mx-auto flex justify-between items-center p-4 shadow-lg top-0 left-0 w-full z-50">
         <h1 className="text-2xl font-bold">My Luxury Travel</h1>
         <button className="md:hidden" onClick={toggleSidebar}>
           <FontAwesomeIcon
