fa6df20e2c1205959ebcabfaf4c1ed7531277398
Rifannvl <riffanafendi@gmail.com>
2024-10-22 09:04:16 +0700

    menambah sekleton

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -1,16 +1,30 @@
-// src/components/Breadcrumbs.js
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
-const Breadcrumbs = ({ items }) => {
-  const location = useLocation(); // Get the current location
+const SkeletonBreadcrumbs = () => (
+  <div className="flex space-x-4 animate-pulse">
+    <div className="h-6 w-24 bg-gray-200 rounded"></div>
+    <div className="h-6 w-24 bg-gray-200 rounded"></div>
+    <div className="h-6 w-24 bg-gray-200 rounded"></div>
+  </div>
+);
+
+const Breadcrumbs = ({ items, loading }) => {
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="container mx-auto rounded-lg shadow-lg border border-gray-200 mt-16 p-4 bg-gradient-to-r from-blue-400 to-purple-500">
+        <SkeletonBreadcrumbs />
+      </div>
+    );
+  }
 
   return (
-    <div className="container mx-auto  rounded-lg shadow-lg border border-gray-200 mt-20">
-      <nav className="p-4 ">
+    <div className="container mx-auto rounded-lg shadow-lg border border-gray-200 mt-16 p-4 bg-white">
+      <nav>
         <ul className="flex space-x-4">
           {items.map((item, index) => {
-            // Check if the current path matches the item's path
             const isActive = location.pathname === item.path;
 
             return (
@@ -22,7 +36,7 @@ const Breadcrumbs = ({ items }) => {
                   to={item.path}
                   className={`font-semibold transition-colors duration-300 ${
                     isActive
-                      ? "text-blue-600"
+                      ? "text-yellow-500" // Ganti warna aktif
                       : "text-gray-800 hover:text-gray-900"
                   }`}
                 >
